be7b2c25a4834a628308f1b548cf66f5bba1902d
reallmerry <166987875+reallmerry@users.noreply.github.com>
2025-06-16 14:53:21 +0400

    Add files via upload

---
+++ application/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Decompiled with CFR 0.153-SNAPSHOT (d6f6758-dirty).
+ */
+package application;
+
+import java.io.IOException;
+
+public class Main {
+    public Main() {
+        Main a2;
+    }
+
+    public static void main(String[] stringArray) {
+        System.out.println("\n################################################\n#                                              #\n#        ## #   #    ## ### ### ##  ###        #\n#       # # #   #   # #  #  # # # #  #         #\n#       ### #   #   ###  #  # # ##   #         #\n#       # # ### ### # #  #  ### # # ###        #\n#                                              #\n# Obfuscation by Allatori Obfuscator v8.7 DEMO #\n#                                              #\n#           http://www.allatori.com            #\n#                                              #\n################################################\n");
+        try {
+            String[] b2;
+            aa.main(b2);
+        } catch (IOException a2) {
+            a2.printStackTrace();
+        }
+    }
+}
+
